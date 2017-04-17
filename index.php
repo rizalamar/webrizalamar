590ebd424ed8ee36f4e5d3f9e92a1abd34164cb5
rizalamar <rizalamarulloh2014@gmail.com>
2017-04-17 17:57:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Science Pedia</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+</head>
+<body>
+<div class="wrap">
+  <p id="options"><a href="Login/">Login</a><a href="contact.php">Contact</a></p>
+  <h1><a href="index.php">Science Pedia</a></h1>
+  <ul id="menu">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="kebijakan-privasi.php">Kebijakan Privasi</a></li>
+  </ul>
+  <div class="clear"></div>
+  <div id="left">
+    <h2><a href="1.php">Pengertian Sel dan teori sel</a></h2>
+    <p>Apa pengertian sel dan bagaimana teori sel? Pertanyaan yang seperti ini yang akan kita jawab di belajarbiologi.com. Kalian pasti sudah tahu apa pengertian sel, oleh karena itu kami tambahkan tentang teori teori sel yang ada dikarenakan dengan mengetahui apa teori sel yang telah ada, maka anda telah memahami apa pengertian sel sebenarnya.</p>
+    <h2><a href="2.php">Langkah-Langkah Metode Ilmiah</a></h2>
+    <p>Langkah langkah metode ilmiah dan contoh metode ilmiah – Metode ilmiah adalah langkah langkah yang ditempuh oleh peneliti dalam menjawab pertanyaan pertanyaan atas masalah masalah dan keingintahuan nya terhadap fenomena fenomena yang terjadi sehingga dihasilkan jawaban yang akurat dan obyektif sehingga mampu diterima secara universal dan dianggap valid.</p>
+    <h2><a href="3.php">Pengertian Bakteriologi</a></h2>
+    <p>Bakteriologi merupakan ilmu yang mempelajari kehidupan dan klasifikasi bakteri, struktur anatomi sel bakteri, cara kerja sel bakteri, interaksi antarsel bakteri, dan juga tanggapan bakteri terhadap perubahan pada lingkungan hidupnya.</p>
+    <h2><a href="4.php">Cuaca dan Iklim</a></h2>
+    <p>Cuaca adalah keadaan atau kelakuan atmosfer pada suatu waktu tertentu yang sifatnya berubah-ubah dari waktu ke waktu. Sedangkan iklim adalah rata-rata keadaan cuaca dalam jangka waktu lama, minimal 30 tahun yang sifatnya tetap.</p>
+    <h2><a href="5.php">Kimia Organik</a></h2>
+    <p>Kimia Organik merupakan cabang kimia yang melibatkan studi tentang struktur, komposisi, dan sintesis senyawa yang mengandung karbon. Dalam memahami bentuk kimia, penting untuk dicatat bahwa semua molekul organik tidak hanya mencakup karbon, tetapi juga hidrogen.</p>
+  </div>
+  <div id="right">
+    <div class="box">
+      <h3>About Science Pedia</h3>
+      <p>Sebuah Website berisi tentang pengetahuan alam umum yang bertujuan untuk membantu anda mencari informasi dengan mudah.</p>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="wrap">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="kebijakan-privasi.php">Kebijakan Privasi</a></li>
+    </ul>
+    <div id="col">
+      <p id="copyright">@ CopyRight: Rizal Amarulloh</p>
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+</body>
+</html>
